45e1d9a503b02c5bd78ea33b5d65fef25fdaab3d
DarkAt26 <84019236+DarkAt26@users.noreply.github.com>
2024-12-05 16:48:38 +0100

    LPTP?

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! IDK WHAT IM DOING");
+Console.WriteLine("Hello, World! IDK WHAT IM DOING, HELL YEAH");
 
 /*
 class HttpServer
