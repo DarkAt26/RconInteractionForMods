dad665a145defbc0a65731cccb747f090c82f53e
DarkAt26 <84019236+DarkAt26@users.noreply.github.com>
2024-12-06 17:48:19 +0100

    frq

--- HttpServer.cs
+++ HttpServer.cs
@@ -15,65 +15,100 @@ namespace HttpRequestRcon
             public string? ip { get; set; }
             public int? port { get; set; }
             public string? key { get; set; }
+            public bool? acceptNonLocalRequests { get; set; }
+        }
+
+        public class HttpRequest
+        {
+            public string? url { get; set; }
+            public string? method { get; set; }
+            public string? userAgent { get; set; }
+            public string? local { get; set; }
         }
 
         public HttpListener? listener;
         public Config? config;
         public int requestCount = 0;
 
+        public void PrintRequestDetails(HttpListenerRequest req)
+        {
+            return;
+            if (req.RawUrl == "/favicon.ico") { return; }
+            // Print out some info about the request
+            Console.WriteLine("Request #{0}", ++requestCount);
+            Console.WriteLine(req.HttpMethod + " request to " + req.Url);
+            Console.WriteLine(req.HttpMethod);
+            Console.WriteLine(req.UserAgent);
+            Console.WriteLine(req.IsLocal == true);
+            Console.WriteLine(req.RawUrl);
+            Console.WriteLine();
+        }
 
-        public async Task HandleIncomingConnections()
+        public string? GetArgument(string argument, string[] arguments)
+        {
+            //Loop through all Arguments
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                //Check if argument from loop starts with argument
+                if (arguments[i].StartsWith(argument))
+                {
+                    //return argument value
+                    return arguments[i].Remove(0, 1 + argument.Length);
+                }
+            }
+            return null;
+        }
+
+        public async Task HandleIncomingRequests()
         {
             bool runServer = true;
 
             // While a user hasn't visited the `shutdown` url, keep on handling requests
             while (runServer)
             {
+                Console.WriteLine();
                 // Will wait here until we hear from a connection
-                HttpListenerContext ctx = await listener.GetContextAsync();
+                HttpListenerContext listnerContext = await listener!.GetContextAsync();
 
                 // Peel out the requests and response objects
-                HttpListenerRequest req = ctx.Request;
-                HttpListenerResponse resp = ctx.Response;
+                HttpListenerRequest req = listnerContext.Request;
+                HttpListenerResponse resp = listnerContext.Response;
 
-                // Print out some info about the request
-                Console.WriteLine("Request #{0}", ++requestCount);
-                Console.WriteLine(req.Url!.ToString());
-                Console.WriteLine(req.HttpMethod);
-                Console.WriteLine(req.UserHostName);
-                Console.WriteLine(req.UserAgent);
-                Console.WriteLine();
+                PrintRequestDetails(req);
 
-                // POST Requests
-                if (req.HttpMethod == "POST" && req.Url.AbsolutePath.StartsWith("/httpRcon"))
+                //Skip Requests which dont target httpRcon & are not GET
+                if (!req.RawUrl!.StartsWith("/httpRcon") && req.HttpMethod == "GET") { continue; }
+
+                //extract command and arguments from RawUrl
+                string relativeUrl = req.RawUrl.Remove(0, 9).ToLower();
+                string command;
+                string[] arguments = { };
+
+                //assign command and arguments to their variables
+                if (relativeUrl.Contains('?'))
                 {
-                    Console.WriteLine(req.Url.AbsolutePath);
-                    Console.WriteLine(req.Url.AbsolutePath.Remove(0, 9));
-
-                    switch (req.Url.AbsolutePath.Remove(0, 9))
-                    {
-                        case "/shutdown":
-                            Console.WriteLine("Shutdown requested");
-                            runServer = false;
-                            break;
-                        case "/restart":
-                            Console.WriteLine("Restart requested");
-                            break;
-                        default:
-                            Console.WriteLine("Unknown request");
-                            break;
-                    }
+                    command = relativeUrl.Split('?')[0].Remove(0, 1);
+                    arguments = relativeUrl.Split('?')[1].Split('&');
                 }
-
-                // GET Requests
-                else if (req.HttpMethod == "GET")
-                {
-
-                }
-
-                // Other Requests
                 else
                 {
+                    command = relativeUrl.Remove(0, 1).ToLower();
+                }
+
+                switch (command)
+                {
+                    case "switchmap":
+                        Rcon.SwitchMap(GetArgument("mapid", arguments), GetArgument("gamemode", arguments));
+                        break;
+
+                    case "shutdown":
+                        Console.WriteLine("Shutdown requested");
+                        runServer = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown request");
+                        break;
                 }
 
                 // Write the response info
@@ -130,10 +165,10 @@ namespace HttpRequestRcon
             listener = new HttpListener();
             listener.Prefixes.Add("http://" + config.ip + ":" + config.port + "/");
             listener.Start();
-            Console.WriteLine("Listening for connections on {0}", "http://" + config.ip + ":" + config.port + "/");
+            Console.WriteLine("Listening for requests on {0}", "http://" + config.ip + ":" + config.port + "/");
 
             // Handle requests
-            Task listenTask = HandleIncomingConnections();
+            Task listenTask = HandleIncomingRequests();
             listenTask.GetAwaiter().GetResult();
 
             // Close the listener
---
+++ Rcon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequestRcon
+{
+    public static class Rcon
+    {
+        public static string? SwitchMap(string? mapId, string? gameMode)
+        {
+            //cancel if arguments are null
+            if (mapId == null || gameMode == null) { return null; }
+
+            Console.WriteLine("SwitchMap " + mapId + " " + gameMode);
+
+            return null;
+        }
+    }
+}
