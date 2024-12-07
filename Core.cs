63c1e0a6187187cf35537ca69ac45ca1826a28da
DarkAt26 <84019236+DarkAt26@users.noreply.github.com>
2024-12-07 06:48:17 +0100

    parts individually kinda working

--- Core.cs
+++ Core.cs
@@ -13,13 +13,44 @@ namespace HttpRequestRcon
             //File.Create("config.txt");
 
             HttpServer server = new HttpServer();
-            server.Start();
+            WebSocketServer ws = new WebSocketServer();
+            RconInteractionForMods rconInteractionForMods = new RconInteractionForMods();
+            //rconInteractionForMods.Start();
+            //ws.Start();
+            //server.Start();
 
             Console.WriteLine("FUCK");
+
+            AsyncTest asyncTest = new AsyncTest();
+            asyncTest.MyMethodAsync(5);
+            asyncTest.MyMethodAsync(2);
+            asyncTest.MyMethodAsync(20);
+
+            while (true) { }
 
             //HttpClient client = new HttpClient();
             //var json = client.GetStringAsync("https://raw.githubusercontent.com/DarkAt26/PavlovVR-RandomBlueprintCollection/refs/heads/main/ModifyItems");
             //Console.WriteLine(json.Result);
+        }
+
+    }
+    class AsyncTest
+    {
+        public async Task MyMethodAsync(int i)
+        {
+            Task<int> longRunningTask = LongRunningOperationAsync(i);
+            // independent work which doesn't need the result of LongRunningOperationAsync can be done here
+
+            //and now we call await on the task 
+            int result = await longRunningTask;
+            //use the result 
+            Console.WriteLine(result);
+        }
+
+        public async Task<int> LongRunningOperationAsync(int i) // assume we return an int from this long running operation 
+        {
+            await Task.Delay(i*1000); // 1 second delay
+            return 1;
         }
     }
 }
---
+++ WebSocketServer.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using System.IO;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+
+namespace HttpRequestRcon
+{
+    class WebSocketServer
+    {
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+
+            server.Start();
+            Console.WriteLine("Server has started on 127.0.0.1:80.{0}Waiting for a connection…", Environment.NewLine);
+
+            TcpClient client = server.AcceptTcpClient();
+
+            Console.WriteLine("A client connected.");
+        }
+    }
+
+    public class RconInteractionForMods
+    {
+        public Config? config;
+        public StreamWriter? commandWriter;
+
+
+
+
+        public async void Start()
+        {
+            Console.WriteLine("Start?");
+
+            config = JsonConvert.DeserializeObject<Config>(System.IO.File.ReadAllText("rifm_config.json"));
+
+            Console.WriteLine(config.IP);
+            Console.WriteLine(config.Port);
+            Console.WriteLine(config.Password);
+
+            Console.WriteLine("Starting");
+
+            // Handle requests
+            Task listenTask = ConnectToRcon();
+            listenTask.GetAwaiter().GetResult();
+
+
+            //Console.WriteLine("Starting");
+            //await ConnectToRcon();
+            Console.WriteLine("Stopped");
+        }
+
+        private Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public async Task ConnectToRcon()
+        {
+            await socket.ConnectAsync(config.IP, config.Port);
+            await using NetworkStream stream = new(socket);
+            using StreamReader reader = new(stream);
+            await using StreamWriter writer = new(stream);
+            await writer.WriteAsync(Convert.ToHexString(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(config.Password))).ToLower());
+            await writer.FlushAsync();
+            commandWriter = writer;
+
+            Console.WriteLine("Connected");
+
+            while (true)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Waited");
+                await ExecuteCommandAsync("UpdateServerName DarkAt26-RandomDynamicName-" + new Random().Next(0,10000000));
+            }
+        }
+
+        public async Task ExecuteCommandAsync(string command)
+        {
+            await commandWriter.WriteAsync(command);
+            await commandWriter.FlushAsync();
+            Console.WriteLine(command);
+        }
+
+        public class Config
+        {
+            public string IP { get; set; } = "";
+            public int Port { get; set; } = 0;
+            public string Password { get; set; } = "";
+        }
+    }
+}
